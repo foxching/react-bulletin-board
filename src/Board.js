7ec177bbce79a658daeda44c01a987bf5d994d85
rechielagria <rechielagria@gmail.com>
2019-05-23 00:56:23 +0800

    Some code modifications

--- src/Board.js
+++ src/Board.js
@@ -1,66 +1,67 @@
-import React, {Component }  from 'react'
-import Note from './Note'
-import {FaPlusCircle} from 'react-icons/fa'
-import uuid  from 'uuid'
-
-
+import React, { Component } from 'react';
+import Note from './Note';
+import { FaPlusCircle } from 'react-icons/fa';
+import uuid from 'uuid';
 
 class Board extends Component {
-	state ={
-		notes:[]
-	}
-	
-	
-	componentWillMount() {
-		var self = this
-		if(this.props.count) {
-			fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
-				.then(response => response.json())
-				.then(json => json[0]
-								.split('. ')
-								.forEach(sentence => self.addNote(sentence.substring(0, 25))))
-		}
-	}
+  state = {
+    notes: []
+  };
 
+  componentWillMount() {
+    var self = this;
+    if (this.props.count) {
+      fetch(
+        `https://baconipsum.com/api/?type=all-meat&sentences=${
+          this.props.count
+        }`
+      )
+        .then(response => response.json())
+        .then(json =>
+          json[0]
+            .split('. ')
+            .forEach(sentence => self.addNote(sentence.substring(0, 25)))
+        );
+    }
+  }
 
-	addNote = (txt) => {
-		this.setState ( {
-			notes:[...this.state.notes, { id:uuid(), note:txt}]
-		})
-	}	
+  addNote = txt => {
+    this.setState({
+      notes: [...this.state.notes, { id: uuid(), note: txt }]
+    });
+  };
 
-	
-	updateNote = (newTxt, note) => {
-		const notes = [...this.state.notes];
-		const index = notes.indexOf(note)
-		notes[index] = {...note}
-		notes[index].note = newTxt
-		this.setState({ notes})
-	}
+  updateNote = (newTxt, note) => {
+    const notes = [...this.state.notes];
+    const index = notes.indexOf(note);
+    notes[index] = { ...note };
+    notes[index].note = newTxt;
+    this.setState({ notes });
+  };
 
-	deleteNote = (id) => {
-		const notes = this.state.notes.filter(note => note.id !== id)
-		this.setState({ notes})
-	}
+  deleteNote = id => {
+    const notes = this.state.notes.filter(note => note.id !== id);
+    this.setState({ notes });
+  };
 
-	render(){
-		return (
-			<div className="board">
-				{
-					this.state.notes.map(note => (
-							<Note 
-								key={note.id} 
-								index={note.id} 
-								note={note}
-								updateNote={this.updateNote}
-								deleteNote={this.deleteNote}
-							/>
-					))
-				}
-				<button onClick ={() => this.addNote("New Note")} id="add"><FaPlusCircle /></button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="board">
+        {this.state.notes.map(note => (
+          <Note
+            key={note.id}
+            index={note.id}
+            note={note}
+            updateNote={this.updateNote}
+            deleteNote={this.deleteNote}
+          />
+        ))}
+        <button onClick={() => this.addNote('New Note')} id="add">
+          <FaPlusCircle />
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Board
+export default Board;
--- src/Note.js
+++ src/Note.js
@@ -1,81 +1,84 @@
-import React, {Component }  from 'react'
-import { FaPen} from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaPen } from 'react-icons/fa';
 import { FaTrash } from 'react-icons/fa';
 import { FaSave } from 'react-icons/fa';
 
-
-
 class Note extends Component {
-    state = {
-        editing:false,
-        noteVal:this.props.note.note
-    }
+  state = {
+    editing: false,
+    noteVal: this.props.note.note
+  };
 
-   componentWillMount() {
-		this.style = {
-			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
-			top: this.randomBetween(0, window.innerHeight - 150, 'px'),
-		    transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
-		}
-	}
+  componentWillMount() {
+    this.style = {
+      right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+      top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+      transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+    };
+  }
 
-	randomBetween(x, y, s) {
-		return x + Math.ceil(Math.random() * (y-x)) + s
-	}
+  randomBetween(x, y, s) {
+    return x + Math.ceil(Math.random() * (y - x)) + s;
+  }
 
-    edit =() => {
-        this.setState({editing:true})
-    }
+  edit = () => {
+    this.setState({ editing: true });
+  };
 
-    save =(e) => {
-        e.preventDefault();
-        const newText = e.target.elements.TextMessage.value
-        this.props.updateNote(newText, this.props.note)
-        this.setState({
-            editing:false
-        })
-    }
+  save = e => {
+    e.preventDefault();
+    const newText = e.target.elements.TextMessage.value;
+    this.props.updateNote(newText, this.props.note);
+    this.setState({
+      editing: false
+    });
+  };
 
-    remove = () => {
-        this.props.deleteNote(this.props.note.id)
-    }
+  remove = () => {
+    this.props.deleteNote(this.props.note.id);
+  };
 
-    onChange =(e) => {
-        this.setState({ noteVal:e.target.value })
-    }
-    
-    renderForm = () => {
+  onChange = e => {
+    this.setState({ noteVal: e.target.value });
+  };
 
-        return (
-                <div className="note" style={this.style}>
-                    <form onSubmit={this.save}>
-                        <textarea type="text" name="TextMessage" value={this.state.noteVal} onChange={this.onChange}/>
-                        <button type="submit" id="save"><FaSave /></button>
-                    </form>
-                </div>
-        );
-    }
+  renderForm = () => {
+    return (
+      <div className="note" style={this.style}>
+        <form onSubmit={this.save}>
+          <textarea
+            type="text"
+            name="TextMessage"
+            value={this.state.noteVal}
+            onChange={this.onChange}
+          />
+          <button type="submit" id="save">
+            <FaSave />
+          </button>
+        </form>
+      </div>
+    );
+  };
 
-    renderDisplay = () => {
+  renderDisplay = () => {
+    return (
+      <div className="note" style={this.style}>
+        <p>{this.props.note.note}</p>
+        <span>
+          <button id="edit" onClick={this.edit}>
+            <FaPen />
+          </button>
+          <button id="remove" onClick={this.remove}>
+            <FaTrash />
+          </button>
+        </span>
+      </div>
+    );
+  };
 
-        return ( 
-                <div className="note" style={this.style}>
-                    <p>{this.props.note.note}</p>
-                    <span>
-                        <button id="edit" onClick={this.edit}><FaPen /></button>
-                        <button id="remove" onClick={this.remove}><FaTrash /></button>
-                    </span>
-                </div>
-        );
-    }
-
-    render(){
-
-        return this.state.editing ? this.renderForm() : this.renderDisplay()
-    }
+  render() {
+    return this.state.editing ? this.renderForm() : this.renderDisplay();
+  }
 }
 
-
-
-
-export default Note
+export default Note;
